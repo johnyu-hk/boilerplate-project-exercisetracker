1d5897ffc9f6f2a66295cf61a211945bbcc9e6a6
John <johnsonyu97@gmail.com>
2024-11-13 02:03:03 +0800

    feat: create a new user

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 const userSchema = new Schema({
   username: { type: String, required: true },
 });
 const users = mongoose.model("users", userSchema);
-
 const exerciseSchema = new Schema({
   username: String,
   description: String,
@@ -23,7 +21,6 @@ const exerciseSchema = new Schema({
   date: String,
 });
 const exercises = mongoose.model("exercises", exerciseSchema);
-
 const logSchema = new Schema({
   username: String,
   count: Number,
@@ -40,46 +37,32 @@ const logs = mongoose.model("logs", logSchema);
 // Express
 app.use(cors());
 // app.use(express.static("/public"));
-// app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json());
 
-app.use("/public", express.static(`${process.cwd()}/public`));
-
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/views/index.html");
-// });
 
-app.get("/api/users/:username", function (req, res) {
-  console.log(req.params);
+app.post("/api/users/", function (req, res) {
   //check if user exist
-  // console.log(req.body.username)
-
-  // console.log("input user name", req.body.username);
-  // users.find({ username: req.body.username })
-  //   .then(data => {
-  //     // check if user exist
-  //     console.log("if user exist",data)
-  //     // if (!data) {
-  //     //   //create user when it is not exist
-  //     //   users.inserOne({ username: req.body.username })
-  //     //   .then(data => {
-  //     //     console.log("created user is ", data)
-  //     //     return res.json(
-  //     //       {"username":data.username,"_id":data._id}
-  //     //     )
-  //     //     })
-  //     // } else {
-  //     //   console.log("user is ", data)
-  //     //   return res.json(
-  //     //     {"username":data.username,"_id":data._id}
-  //     //   )
-  //     // }
-  //     })
+  console.log("input user name", req.body.username);
+  users.findOne({ username: req.body.username }).then((data) => {
+    // check if user exist
+    console.log("if user exist", data);
+    if (!data) {
+      //create user when it is not exist
+      users.create({ username: req.body.username }).then((data) => {
+        console.log("created user is ", data);
+        return res.json({ username: data.username, _id: data._id });
+      });
+    } else {
+      console.log("user is ", data);
+      return res.json({ username: data.username, _id: data._id });
+    }
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
